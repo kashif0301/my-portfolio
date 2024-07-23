0659afc8389a1bbe9b49de781e5c2ccb8c076dc5
Kashif <kashifnawaz0301@gmail.com>
2024-07-23 05:13:11 -0700

    fixed more errors

--- src/app/components/green.js
+++ src/app/components/green.js
@@ -1,10 +1,10 @@
 "use client"
-import React, from "react";
+import React, {useState, useEffect} from "react";
 import Image from "next/image";
 import Styles from "../styles/green.module.css";
 
 const Green = () => {
- 
+
   
 
   return (
